61c74cddd4c53f8ac54c6efe14ac14b1be7b35c1
Student <student@GAs-MacBook-Air-3.local>
2017-05-17 23:27:06 -0400

    Finished reordering code. Working on move list

--- src/pokedex.js
+++ src/pokedex.js
@@ -5,12 +5,11 @@ import axios from 'axios';
 class Pokedex extends Component {
   constructor(props) {
     super(props);
-    this.state = { apiData: [], current: [], };
+    this.state = { apiData: [], current: [] };
   }
 
   componentDidMount() {
     axios.get("http://pokeapi.co/api/v2/pokemon/?limit=151").then((allData) => {
-        console.log(allData.data);
         this.setState({ apiData: allData.data.results });
      });
   }
@@ -36,20 +35,55 @@ class Pokedex extends Component {
   }
 
   createPokemonProfile() {
-    console.log(this.createNumber(this.state.current.id), (this.state.current));
     this.indents = [];
 
     if(this.state.current.id !== undefined) {
       this.indents.push(<p key="a" className="monEntry">{this.createNumber(this.state.current.id) + this.capitalize(this.state.current.name)}</p>);
       this.indents.push(<p key="b" className="monEntry">Weight: {this.state.current.weight}</p>);
-      this.indents.push(<p key="c" className="monEntry">Type(s): {this.checkData("Type", this.state.current, this.state.current.types.length)}</p>);
-      this.indents.push(<p key="d" className="monEntry">Abilities: {this.checkData("Ability", this.state.current, this.state.current.abilities.length)}</p>);
-      this.indents.push(<p key="e" className="monEntry">All Moves: {this.checkData("Moves", this.state.current, this.state.current.moves.length)}</p>);
+      this.indents.push(<p key="c" className="monEntry">Type(s): {this.checkData("Type", this.state.current.types.length)}</p>);
+      this.indents.push(<p key="d" className="monEntry">Abilities: {this.checkData("Ability", this.state.current.abilities.length)}</p>);
+      this.indents.push(<p key="e" className="monEntry">All Moves: {this.checkData("Moves", this.state.current.moves.length)}</p>);
     }
   
     return this.indents;
   }
   
+  createNumber(pokemonIndex) {
+    this.fullString = '' + pokemonIndex;
+    
+    while (this.fullString.length < 3) {
+        this.fullString = '0' + this.fullString;
+    }
+
+    return this.fullString += ". ";
+  }
+
+  capitalize(thisName) {
+    return thisName.charAt(0).toUpperCase() + thisName.slice(1);
+  }
+
+  checkData(path, dataLength) {
+    this.fullString = '';
+
+    for (var i = 0; i < dataLength; i++) {
+      this.sep = (i + 1 < dataLength) ? " / " : "";
+      this.fullString += this.pickPath(path, this.state.current, i);
+    }
+
+    return this.fullString;
+  }
+
+  pickPath(chosenPathway, apiInfo, i) {
+    switch(chosenPathway) {
+        case "Type":
+          return this.capitalize(apiInfo.types[i].type.name) + this.sep;
+        case "Ability":
+          return this.capitalize(apiInfo.abilities[i].ability.name) + this.sep;
+        default: // Moves
+          return this.capitalize(apiInfo.moves[i].move.name) + this.sep;
+    }
+  }
+
   render() {
     return (
       <div>
@@ -57,48 +91,11 @@ class Pokedex extends Component {
         <div className="columns monList">
           {this.createPokemonList()}
         </div>
-
         <div className="columns monProfile">
           {this.createPokemonProfile()}
         </div>
       </div>
     );
-  }
-
-  createNumber(pokemonIndex) {
-    var fullString = '' + pokemonIndex;
-    
-    while (fullString.length < 3) {
-        fullString = '0' + fullString;
-    }
-
-    return fullString += ". ";
-  }
-
-  capitalize(thisName) {
-    return thisName.charAt(0).toUpperCase() + thisName.slice(1);
-  }
-
-  checkData(path, typeData, dataLength) {
-    var fullString = '';
-
-    for (var i = 0; i < dataLength; i++) {
-      var sep = (i + 1 < dataLength) ? " / " : "";
-      fullString += this.pickPath(path, typeData, i, sep);
-    }
-
-    return fullString;
-  }
-
-  pickPath(chosenPathway, apiInfo, i, sep) {
-    switch(chosenPathway) {
-        case "Type":
-          return this.capitalize(apiInfo.types[i].type.name) + sep;
-        case "Ability":
-          return this.capitalize(apiInfo.abilities[i].ability.name) + sep;
-        default: // Moves
-          return this.capitalize(apiInfo.moves[i].move.name) + sep;
-    }
   }
 }
 
